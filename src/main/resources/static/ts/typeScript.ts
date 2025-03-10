f341629ffbdf542ca5969f4c3627c112d9f87b0b
rememberName <ri.eaner@hotmail.com>
2025-03-10 18:10:14 +0900

    B-1-5課題完成

---
+++ src/main/resources/static/ts/typeScript.ts
@@ -0,0 +1,119 @@
+// fetchでデータを取得
+fetch('/api/users/')  // 請求したいcontrollerクラスのurl
+	.then(response => response.json()) // 戻ったデータを解析
+	.then(userList => {
+		// ここで戻ったデータをJSPに渡す
+		console.log("data");
+		displayData(userList);
+	}).catch(error => console.error('Error fetching data:', error)); // 異常処理
+
+// jspでデータを展示する。EL式forEachでいいですか、ここは練習のため、HTMLここで作成
+function displayData(userList: Array<{ userName: string, userSex: string, address: string }>) {
+	// idによってDomを取得
+	const userListElement = document.getElementById("data");
+	// dom取れるなら、データを展示
+	if (userListElement) {
+		userList.forEach(user => {
+			const showItem = document.createElement("tr");
+			showItem.innerHTML = `<td>${user.userName}</td><td>${user.userSex}</td><td>${user.address}</td>`;
+			userListElement.appendChild(showItem);
+		})
+	}
+}
+
+// 非同期関数を定義
+/*async function fatchData() {
+	let data = await fetch('/api/users/');
+	let json = await data.json();
+	displayData(json);
+}*/
+
+// interface練習
+interface Person {
+	name: string;// string型なname
+	age: number;// number型なage
+}
+
+
+// class練習
+class Student implements Person {
+	name = "ss";
+	age = 14;
+}
+
+// 再利用できるメソッド（引数名:引数の型）:戻り値の型
+function greet(student: Student): Student {
+	student.name = "マリア";
+	return student;
+}
+
+// type練習
+type Animal = { name: string; sound: string }; // 文字列型な名前と文字列な声の動物型
+
+
+//　オブジェクト指向
+class Dog {
+	// 変更不可なプロパティ
+	readonly dogId: number = 110;
+	private animal: Animal;
+	private age: number;
+
+	//初期化メソッド
+	constructor(dogId: number) {
+		this.dogId = dogId;
+		this.animal = { name: "犬", sound: "ワンワン" }; // 初期化
+		this.age = 0; // 初期値
+	}
+
+	// setメッソド
+	setAge(age: number): void {
+		this.age = age;
+	}
+	// getメソッド
+	getAge(): number {
+		return this.age;
+	}
+	// getAnimal
+	getAnimal(): Animal {
+		return this.animal;
+	}
+	// getDogId
+	getDogId(): number {
+		return this.dogId;
+	}
+}
+
+class Main {
+
+	student: Student;
+	dog: Dog;
+
+	//　初期化メソッド
+	constructor() {
+		this.student = new Student();
+		this.dog = new Dog(110);
+	}
+
+	execute() {
+		console.log("studentクラス");
+		console.log(`${this.student.name} -> ${this.student.age}`);
+
+		const maria = greet(this.student);
+		console.log("greenメソッド");
+		console.log(`${maria.name} -> ${maria.age}`);
+
+		this.dog.setAge(10);
+		console.log("dogクラス");
+		console.log(this.dog.getAnimal().name);
+		console.log(this.dog.getAnimal().sound);
+		console.log(this.dog.getAge());
+		console.log(this.dog.getDogId());
+	}
+}
+
+// 実行
+const mainInstance = new Main();
+mainInstance.execute();
+
+
+
---
+++ src/main/webapp/WEB-INF/jsp/typeScript.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script src="/ts/typeScript.js"></script> 
+</head>
+<body>
+<table id="data">
+<tr>
+<th>名前</th>
+<th>性別</th>
+<th>アドレス</th>
+</tr>
+</table>
+</body>
+</html>
