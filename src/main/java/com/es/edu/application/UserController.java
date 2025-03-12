9662c9ee5851aed8b4ff90b05a85bbc686aaab16
rememberName <ri.eaner@hotmail.com>
2025-03-12 10:51:37 +0900

    コメッドを追加

--- src/main/java/com/es/edu/application/UserController.java
+++ src/main/java/com/es/edu/application/UserController.java
@@ -22,7 +22,9 @@ import com.es.edu.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 /**
- * ユーザー情報操作コントローラクラス。
+ * [概要] <p>ユーザー情報操作コントローラクラス。</p>
+ * [説明] <p>ユーザー情報操作コントローラクラス。</p>
+ * [補充] <p>特になし。</p>
  */
 @RestController
 @RequestMapping("/api/users")
@@ -33,11 +35,15 @@ public class UserController {
     private final UserService userService;
 
     /**
-     * ユーザー情報を全て取得。
-     * @return ユーザー情報リスト
+     * [概要] <p>ユーザー情報を全て取得。</p>
+     * [説明] <p>ユーザー情報を全て取得。</p>
+     * [補充] <p>特になし。</p>
+     *
+     * @return ユーザーフォーム
      */
     @GetMapping
     public UserOutputForm getAllUsers() {
+    	
     	List<UserInfoOutputDto> userInfoOutputDtos = userService.selectAll();
     	UserOutputForm userForm = new UserOutputForm();
     	userForm.setUserInfoOutputList(userInfoOutputDtos);
@@ -45,12 +51,16 @@ public class UserController {
     }
 
     /**
-     * ユーザー情報を取得（ユーザーIDによる検索）。
+     * [概要] <p>ユーザー情報を取得（ユーザーIDによる検索）。</p>
+     * [説明] <p>。</p>
+     * [補充] <p>特になし。</p>
+     *
      * @param userId ユーザーID
      * @return ユーザー情報
      */
     @GetMapping("/{userId}")
     public ResponseEntity<UserOutputForm> getUserById(@PathVariable Integer userId) {
+    	
     	UserInfoOutputDto userInfoOutputDto = userService.selectUserEntityById(userId);
         UserOutputForm userForm = new UserOutputForm();
         List<UserInfoOutputDto> userInfoOutputDtos = new ArrayList<>();
@@ -64,9 +74,12 @@ public class UserController {
     }
 
     /**
-     * ユーザー情報を更新（ユーザーIDによる更新）または追加。
-     * @param userInfoInputDto ユーザー情報
-     * @return 更新結果メッセージ
+     * [概要] <p>ユーザー情報を追加。</p>
+     * [説明] <p>ユーザー情報を追加。</p>
+     * [補充] <p>特になし。</p>
+     *
+     * @param userInputFrom ユーザー情報
+     * @return　ユーザーフォーム
      */
     @PutMapping("/addUser")
     public UserOutputForm addUser(@RequestBody UserInputFrom userInputFrom) {
@@ -87,9 +100,12 @@ public class UserController {
     }
 
     /**
-     * ユーザー情報を更新（ユーザーIDによる更新）。
-     * @param userInfoInputDto ユーザー情報
-     * @return 更新結果メッセージ
+     * [概要] <p>ユーザー情報を更新。</p>
+     * [説明] <p>ユーザー情報を更新（ユーザーIDによる更新）。</p>
+     * [補充] <p>特になし。</p>
+     *
+     * @param userInputFrom　ユーザー情報
+     * @return ユーザーフォーム
      */
     @PutMapping("/updateUser")
     public UserOutputForm updateUser(@RequestBody UserInputFrom userInputFrom) {
@@ -110,12 +126,17 @@ public class UserController {
     }
 
     /**
-     * ユーザー情報を削除（ユーザーIDによる削除）。
+     * [概要] <p>ユーザー情報を削除</p>
+     * [説明] <p>ユーザー情報を削除（ユーザーIDによる削除）。</p>
+     * [補充] <p>特になし。</p>
+     *
      * @param userEntityId ユーザーID
-     * @return 削除結果メッセージ
+     * @return ユーザーフォーム
      */
     @DeleteMapping("/{userEntityId}")
     public UserOutputForm deleteUser(@PathVariable Integer userEntityId) {
+    	
+    	// 削除処理
         int rows = userService.deleteUserEntity(userEntityId);
         UserOutputForm userForm = new UserOutputForm();
         userForm.setRows(rows);
